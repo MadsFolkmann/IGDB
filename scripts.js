0785949b89931abcec1b9c1167afcd3ca530f3f1
Abdiox <abdirahmanroble2610@gmail.com>
2023-05-03 13:51:06 +0200

    lavet filter knap

--- scripts.js
+++ scripts.js
@@ -10,9 +10,33 @@ async function initApp() {
 
   document.querySelector("#btn-create-game").addEventListener("click", showGameModal);
   document.querySelector("#form-delete-game").addEventListener("submit", deleteGameClicked);
-  document.querySelector("#form-delete-game").addEventListener("click", deletePostClickedNo);
-    //Update//
-     document.querySelector("#form-update-game").addEventListener("submit", updateGameClicked);
+  document.querySelector("#form-delete-game").addEventListener("click", deleteGameClickedNo);
+  //Update//
+  document.querySelector("#form-update-game").addEventListener("submit", updateGameClicked);
+
+  document.querySelector("#input-search").addEventListener("keyup", inputSearchChanged);
+  document.querySelector("#input-search").addEventListener("search", inputSearchChanged);
+}
+
+// ---------------------filter games-----------------------//
+
+function inputSearchChanged(event) {
+  const value = event.target.value;
+  const postsToShow = searchGames(value);
+  displayGames(postsToShow);
+}
+
+function searchGames(searchValue) {
+  searchValue = searchValue.toLowerCase();
+
+  const results = games.filter(checkTitle);
+
+  function checkTitle(game) {
+    const title = game.title.toLowerCase();
+    return title.includes(searchValue);
+  }
+
+  return results;
 }
 
 // ---------------------Create User/Posts-----------------------//
@@ -23,8 +47,8 @@ function showGameModal() {
 
   dialog.showModal();
 
-    document.querySelector("#createGame").addEventListener("submit", createGameClicked);
-    
+  document.querySelector("#createGame").addEventListener("submit", createGameClicked);
+
   // closes dialog when clicking outside the dialog
 }
 
@@ -74,10 +98,10 @@ function deleteGameClicked(event) {
   console.log(event);
   const id = event.target.getAttribute("data-id");
   deleteGame(id);
-  document.querySelector("dialog-delete-game").close();
+  document.querySelector("#dialog-delete-game").close();
 }
 
-function deletePostClickedNo() {
+function deleteGameClickedNo() {
   console.log("Close delete dialog");
   document.querySelector("#dialog-delete-game").close();
 }
@@ -126,8 +150,8 @@ function showGames(gameObject) {
     </article>
   `;
 
-    document.querySelector("#games").insertAdjacentHTML("beforeend", html);
-    document.querySelector("#games article:last-child .btn-update").addEventListener("click", () => updateClicked(gameObject))
+  document.querySelector("#games").insertAdjacentHTML("beforeend", html);
+  document.querySelector("#games article:last-child .btn-update").addEventListener("click", () => updateClicked(gameObject));
   document.querySelector("#games article:last-child .btn-delete").addEventListener("click", deleteClicked);
 
   function deleteClicked() {
@@ -139,46 +163,45 @@ function showGames(gameObject) {
 }
 
 function updateClicked(gameObject) {
-    const updateForm = document.querySelector("#form-update-game")
-    
-    updateForm.title.value = gameObject.title
-    updateForm.image.value = gameObject.image
-    updateForm.resume.value = gameObject.resume
-    updateForm.genre.value = gameObject.genre
-    updateForm.rating.value = gameObject.rating
-    document.querySelector("#form-update-game").setAttribute("data-id", gameObject.id);
-      document.querySelector("#dialog-update-game").showModal();
+  const updateForm = document.querySelector("#form-update-game");
+
+  updateForm.title.value = gameObject.title;
+  updateForm.image.value = gameObject.image;
+  updateForm.resume.value = gameObject.resume;
+  updateForm.genre.value = gameObject.genre;
+  updateForm.rating.value = gameObject.rating;
+  document.querySelector("#form-update-game").setAttribute("data-id", gameObject.id);
+  document.querySelector("#dialog-update-game").showModal();
 }
 
 function updateGameClicked(event) {
-    event.preventDefault();
-    const form = event.target;
-    const id = event.target.getAttribute("data-id");
+  event.preventDefault();
+  const form = event.target;
+  const id = event.target.getAttribute("data-id");
 
-    const title = form.title.value;
-    const rating = form.rating.value;
-    const image = form.image.value;
+  const title = form.title.value;
+  const rating = form.rating.value;
+  const image = form.image.value;
 
-    updateGame(id, title, rating, image);
-    document.querySelector("#dialog-update-game").close();
+  updateGame(id, title, rating, image);
+  document.querySelector("#dialog-update-game").close();
 }
 
 async function updateGame(id, title, rating, image) {
-    const updatedGame = {
-        title: title,
-        rating: rating,
-        image: image,
-    };
+  const updatedGame = {
+    title: title,
+    rating: rating,
+    image: image,
+  };
 
-    const json = JSON.stringify(updatedGame);
-    const response = await fetch(`${endpoint}/games/${id}.json`, { method: "PUT", body: json });
+  const json = JSON.stringify(updatedGame);
+  const response = await fetch(`${endpoint}/games/${id}.json`, { method: "PUT", body: json });
 
-    if (response.ok) {
-        console.log("Game succesfully updated in firbase🐱🐱");
+  if (response.ok) {
+    console.log("Game succesfully updated in firbase🐱🐱");
     updateGrid();
   }
 }
-
 
 //-------Refresh ved click af IGDB-------//
 const igdbImg = document.querySelector("#igdb-img");
